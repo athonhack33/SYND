8b05394c4d2dbf422820c7017ede220cb0f91cbf
mephisto <athonhack33@gmail.com>
2019-08-30 22:02:32 +0530

    SignUp_LogOut_NewUserCreate_tested

--- app/src/main/java/com/example/synd/MainActivity.java
+++ app/src/main/java/com/example/synd/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.google.firebase.auth.FirebaseAuth;
 
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public Button btnLogOut;
     ImageView i1,i2,i3,i4,i5;
 
     @Override
@@ -21,12 +25,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         i3 = findViewById(R.id.autogen);
         i4 = findViewById(R.id.rewards);
         i5 = findViewById(R.id.SOS);
+        btnLogOut = findViewById(R.id.btnLogOut);
 
         i1.setOnClickListener(this);
         i2.setOnClickListener(this);
         i3.setOnClickListener(this);
         i4.setOnClickListener(this);
         i5.setOnClickListener(this);
+
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(MainActivity.this, ActivityLogin.class);
+                startActivity(I);
+            }
+        });
 
     }
 
