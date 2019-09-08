a9758ff262d4e114b4b72186e53f52cea74391ee
mephisto <athonhack33@gmail.com>
2019-09-08 16:51:52 +0530

    UI_Update_2

--- app/src/main/java/com/example/synd/SplashScreenActivity.java
+++ app/src/main/java/com/example/synd/SplashScreenActivity.java
@@ -8,12 +8,14 @@ import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide(); // hide the title bar
 
         new Handler().postDelayed(new Runnable() {
             @Override
